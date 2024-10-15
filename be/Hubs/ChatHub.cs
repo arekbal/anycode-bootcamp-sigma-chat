b2f7097e0f4fac62e82bca30ebef00cc016bc309
Arek Bal <arek_bal@wp.pl>
2024-10-15 23:12:04 +0200

    init

---
+++ be/Hubs/ChatHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace be.Hubs;
+
+public class ChatHub : Hub
+{
+  public async Task NewMessage(string username, string message) =>
+      await Clients.All.SendAsync("messageReceived", username, message);
+}
