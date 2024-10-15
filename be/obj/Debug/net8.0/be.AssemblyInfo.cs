4f44b8557f171d88f72c5b6ad71747098ae4775b
Arek Bal <arek_bal@wp.pl>
2024-10-15 23:38:00 +0200

    some updates

--- be/obj/Debug/net8.0/be.AssemblyInfo.cs
+++ be/obj/Debug/net8.0/be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c8b21453c1d775bfcd99f44fb106c9811e9d59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c8a9b7cde868224f2be6dd57fb438370377166")]
 [assembly: System.Reflection.AssemblyProductAttribute("be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
